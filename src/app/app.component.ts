c54dc7acc5f6ca8e8eded86e351966b699bb5d1a
Javes <vanaartsenj@gmail.com>
2017-03-21 07:17:43 -0500

    ion-slides for sidescroller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,9 @@ rootPage = StartPage;
     this.pages = [
       { title: 'Home', component: Home},
       { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'Page Two', component: Page2 },
+      { title: 'Start', component: StartPage }
+
     ];
     console.log(this.pages);
   }
