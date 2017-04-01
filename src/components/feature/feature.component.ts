5f8858644dc4990071ef0d80e891066857ce3d59
Javes <vanaartsenj@gmail.com>
2017-03-31 21:00:32 -0500

    checkpoint

--- src/components/feature/feature.component.ts
+++ src/components/feature/feature.component.ts
@@ -5,7 +5,7 @@
  * Created by Javes on 3/19/2017.
  */
 import { Component } from '@angular/core';
-
+import { AzureService } from '../../services/azure.service';
 
 
 @Component({
@@ -13,9 +13,20 @@ import { Component } from '@angular/core';
   templateUrl: 'feature.component.html'
 })
 export class Feature {
+  public video_ids: any[];
 
-  constructor() {
+  constructor(
+    private azureService: AzureService
+  ) {
 
   }
 
+  ngOnInit() {
+    this.azureService.getFeaturedVideoIds()
+      .then((resp) => {
+        console.log('feature component: ', resp);
+      }, (resp) => {
+        console.log('feature component the second time: ', resp);
+      });
+  }
 }
