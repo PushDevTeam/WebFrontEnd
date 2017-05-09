74b75f88505e7019d865db490999422f3a661b0e
Javes <vanaartsenj@gmail.com>
2017-05-09 09:55:53 -0500

    sidescroller getting there

--- src/components/side-scroller/side-scroller.component.ts
+++ src/components/side-scroller/side-scroller.component.ts
@@ -1,7 +1,8 @@
 /**
  * Created by Javes on 3/19/2017.
  */
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewChild} from '@angular/core';
+import {Slides} from 'ionic-angular';
 import {VideoInfoService} from '../../services/video-info.service';
 import {UserService} from '../../services/user.service';
 
@@ -12,27 +13,42 @@ import {UserService} from '../../services/user.service';
 })
 
 export class SideScroller {
+  @ViewChild(Slides) slides: Slides;
   @Input() set_label: string;
   public v_ids: string[];
 
 
 
- constructor(private videoInfoService: VideoInfoService,
-   private userService: UserService) {
+  constructor(private videoInfoService: VideoInfoService,
+    private userService: UserService) {
 
-     this.videoInfoService.getAllVideoIds().then( (res) => {
-       this.v_ids = res;
-     });
- }
+    this.videoInfoService.getAllVideoIds().then((res) => {
+      this.v_ids = res;
+    });
+  }
+
+  ngAfterViewInit(){
+    this.slides.lockSwipes(true);
+
+  }
 
 
+  vIdsPromise() {
+    return this.videoInfoService.getAllVideoIds();
+  }
 
+  goToCategoryView() {
+    alert("goToCategoryView: " + this.set_label);
+  }
+  slideLeft(){
+    this.slides.lockSwipes(false);
+    this.slides.slidePrev();
+    this.slides.lockSwipes(true);
+  }
+  slideRight(){
+    this.slides.lockSwipes(false);
+    this.slides.slideNext();
+    this.slides.lockSwipes(true);
+  }
 
- vIdsPromise() {
-   return this.videoInfoService.getAllVideoIds();
- }
-
- goToCategoryView(){
-   alert("goToCategoryView: "+ this.set_label);
- }
 }
