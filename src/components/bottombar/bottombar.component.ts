aa92de26d5234dee704cae00bc6d77bb4650895e
wayne <wayneeusa@live.com>
2017-05-14 23:55:11 -0500

    work on choose station popup

--- src/components/bottombar/bottombar.component.ts
+++ src/components/bottombar/bottombar.component.ts
@@ -10,6 +10,30 @@ import {Component} from '@angular/core'
 })
 
 export class BottomBar {
+  private stationPopupActive: boolean = false;
   constructor(){
   }
+
+  onStationPopup() {
+    let stationMenu = document.getElementById('station-menu');
+    let stationButton = document.getElementById('station-button');
+    if (this.stationPopupActive) {
+      stationButton.classList.remove('active');
+      stationMenu.classList.remove('station-popup-active');
+      this.stationPopupActive = false;
+    } else {
+      stationButton.classList.add('active');
+      stationMenu.classList.add('station-popup-active');
+      this.stationPopupActive = true;
+    }
+  }
+
+  createStation() {
+
+  }
+
+  seeAllStations() {
+
+  }
+
 }
