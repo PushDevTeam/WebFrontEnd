a21586d6c64f42d2451b9731fe1ade4946b78e3c
Ante Grgic <ante16@gmail.com>
2017-05-16 13:53:42 -0500

    pulled in volumn bar updates

--- src/components/bottombar/bottombar.component.ts
+++ src/components/bottombar/bottombar.component.ts
@@ -33,13 +33,17 @@ export class BottomBar {
    })
   }
 
-  updateData = (e: any) => {
+  updateData = () => {
     this.totalTime = this.audioElement.duration;
   };
 
-  updateTime = (e: any) => {
+  updateTime = () => {
     this.currentTime = this.audioElement.currentTime;
   };
+
+  updateVol() {
+    this.audioElement.volume =  (<HTMLInputElement>document.getElementById("volume-slider")).value;
+  }
 
   onStationPopup() {
     let stationMenu = document.getElementById('station-menu');
